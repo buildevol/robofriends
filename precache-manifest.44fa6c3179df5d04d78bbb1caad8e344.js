925116d4c43b3727e3edd6e8dde52c473d9ca05f
buildevol <buildevol@outlook.com>
2020-05-07 22:48:27 +0800

    Updates

---
+++ precache-manifest.44fa6c3179df5d04d78bbb1caad8e344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ff494d613efe053092d5b6e97b7eb6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "79371d362454de0551d7",
+    "url": "/robofriends/static/css/2.ceea4331.chunk.css"
+  },
+  {
+    "revision": "26067e014d3ac753ed24",
+    "url": "/robofriends/static/css/main.2518a1c9.chunk.css"
+  },
+  {
+    "revision": "79371d362454de0551d7",
+    "url": "/robofriends/static/js/2.5773fc32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.5773fc32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26067e014d3ac753ed24",
+    "url": "/robofriends/static/js/main.72a672f6.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
